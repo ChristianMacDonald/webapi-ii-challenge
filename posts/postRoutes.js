02aab24332d04d6c835ab695a9a47d8dcc4f7fb5
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-27 14:28:52 -0400

    Added post request to /api/posts endpoint

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -3,11 +3,26 @@ const db = require('../data/db');
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+    if (req.body.title && req.body.contents) {
+        db.insert(req.body).then(post => {
+            db.findById(post.id).then(post => {
+                res.status(201).json(post);
+            });
+        }).catch(error => {
+            res.status(500).json({ error: 'There was an error while saving the post to the database.' });
+        });
+    } else {
+        res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+    }
+})
+
 router.get('/', (req, res) => {
     db.find().then(posts => {
+        console.log(posts);
         res.status(200).json(posts);
     }).catch(error => {
-        res.status(500).json({ errorMessage: 'Failed to retrieve posts!' });
+        res.status(500).json({ error: 'The posts information could not be retrieved.' });
     });
 });
 
