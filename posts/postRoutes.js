4f84cab3990f1afc7dd7f825fbd25194bab2b10c
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-27 14:42:11 -0400

    Added a get request for the /api/posts/:id endpoint

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -26,4 +26,16 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    db.findById(req.params.id).then(post => {
+        if (post !== []) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+        }
+    }).then(error => {
+        res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+})
+
 module.exports = router;
