00ee472af6e324aeca93183fb3cea52ef33c5601
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-27 15:07:50 -0400

    Added get request for /api/posts/:id/comments endpoint and fixed get request for /api/posts/:id endpoint

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -45,13 +45,29 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     db.findById(req.params.id).then(post => {
-        if (post !== []) {
+        if (post && post.length) {
             res.status(200).json(post);
         } else {
             res.status(404).json({ message: 'The post with the specified ID does not exist.' });
         }
     }).then(error => {
         res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+})
+
+router.get('/:id/comments', (req, res) => {
+    db.findById(req.params.id).then(post => {
+        if (post && post.length) {
+            db.findPostComments(req.params.id).then(comments => {
+                res.status(200).json(comments);
+            }).catch(error => {
+                res.status(500).json({ error: 'The comments information could not be retrieved.' });
+            });
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+        }
+    }).catch(error => {
+        res.status(500).json({ error: 'The comments information could not be retrieved.' });
     });
 })
 
